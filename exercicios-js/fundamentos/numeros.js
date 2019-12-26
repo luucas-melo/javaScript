4d8075e883e6037c369aea61516a81dec335e659
luucasxd <lucasmelodos322@gmail.com>
2019-12-26 19:19:43 -0300

    testando os numeros

---
+++ exercicios-js/fundamentos/numeros.js
@@ -0,0 +1,13 @@
+const peso1 = 1.0
+const peso2 = Number('3.0')
+
+console.log(peso1, peso2)
+console.log(Number.isInteger(peso1))
+
+const prova1 = 9.83
+const prova2 = 6.87
+
+const total = prova1 * peso1 + prova2 * peso2
+let media = total / (peso1+peso2)
+media = media.toFixed(2)
+console.log(media)
